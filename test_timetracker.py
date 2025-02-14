31dc291f80b1f5ea5410fea62fe654c1b32a8b6f
Jim Kraxberger <jkraxberger@schoolsfirstfcu.org>
2025-02-14 09:53:13 -0800

    added identify decorator with verbose parameter

--- test_timetracker.py
+++ test_timetracker.py
@@ -1,8 +1,16 @@
+import sys
 import time as t
 import timetracker as tt
 import unittest
 
-class TestMain(unittest.TestCase):
+class TestTimeTracker(unittest.TestCase):
+    VERBOSE = False
+    def identify(func):
+        def wrapper(*args, **kwargs):
+            if TestTimeTracker.VERBOSE: print(f"\nTEST: {func.__name__}")
+            return func(*args, **kwargs)
+        return wrapper
+
     def setUp(self):
         self.timer = tt.TimeTracker()
         t.sleep(3)
@@ -18,6 +26,13 @@ class TestMain(unittest.TestCase):
         with self.subTest(timevalue=timevalue):
             self.assertEqual(timevalue, "00:00:03.000")
 
+if __name__ == "__main__":
+    if len(sys.argv) > 1 and sys.argv[1] == '-v':   
+        TestTimeTracker.VERBOSE = True
+    else:
+        TestTimeTracker.VERBOSE = False
+        
+    unittest.main()
 # # Example Usage
 # timer = ProcessTimer()
 # timer.start()
